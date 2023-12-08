629dd692204773c893638e7ddb8c7dcabe1cfda0
Aniketh Nayak N <nayakaniketh970@gmail.com>
2023-12-08 15:02:38 +0530

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./NavBar.css"
+function NavBar() {
+  return (
+    <div>
+        <h1>UNified Health Interface</h1>
+
+    </div>
+  )
+}
+
+export default NavBar
